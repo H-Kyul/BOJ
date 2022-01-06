b00ebcfc9e6565ae36fc7d397f2b5e636b1ca311
Hankyul Kim <71064836+Hankyul-k@users.noreply.github.com>
2022-01-06 15:18:59 +0900

    백준 1550번 16진수
    수학(진수 변환), 구현

---
+++ bronze5/1550_16진수.py
@@ -0,0 +1,12 @@
+'''
+https://www.acmicpc.net/problem/1550
+'''
+
+# 제출 답안 -- int 사용 
+print(int(input(),16))
+
+
+# 진수 변환
+# 1. 10진수로 >> int(다른진수값,진수)
+# 2. format 사용 >> format(값,진수)
+# 진수 >> #b : 2진수, #o : 8진수, #x : 16진수, #d : 10진수
