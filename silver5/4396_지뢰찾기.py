1258a9f6a5b6a99fcc660bde96bea139f2435bee
Hankyul Kim <71064836+Hankyul-k@users.noreply.github.com>
2022-01-18 17:35:39 +0900

    백준 4396번 지뢰 찾기
    구현, 문자열, 파싱

--- silver5/4396_지뢰찾기.py
+++ silver5/4396_지뢰찾기.py
@@ -2,6 +2,35 @@
 https://www.acmicpc.net/problem/4396
 '''
 
+# 제출 답안 2 - 바로 출력, 지뢰 밟았는지 먼저 체크
+n = int(input())
+map = [list(input()) for _ in range(n)]
+visited = [list(input()) for _ in range(n)]
+bomb = 0
+
+for i in range(n):
+    for j in range(n):
+        if visited[i][j]=='x' and map[i][j]=='*':
+            bomb = 1
+            break
+for i in range(n):
+    for j in range(n):
+        cnt = 0
+        if bomb and map[i][j] == '*':
+            print('*',sep='',end='')
+            continue
+        if visited[i][j]=='.':
+            print('.',sep='',end='')
+            continue
+        for x in range(i-1,i+2):
+            for y in range(j-1,j+2):
+                if 0<=x<n and 0<=y<n:
+                    if map[x][y] == '*':
+                        cnt += 1
+        print(cnt,sep='',end='')
+    print()
+
+
 # 제출 답안
 n = int(input())
 map = [list(input()) for _ in range(n)]
