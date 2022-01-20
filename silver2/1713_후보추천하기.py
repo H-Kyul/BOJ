ff215084196107a9892ddf3e431e1829f2e85cf0
Hankyul Kim <71064836+Hankyul-k@users.noreply.github.com>
2022-01-21 00:20:58 +0900

    백준 1713번 후보 추천하기
    구현

--- silver2/1713_후보추천하기.py
+++ silver2/1713_후보추천하기.py
@@ -1,6 +1,30 @@
+'''
+https://www.acmicpc.net/problem/1713
+'''
 
 
-# 제출 답안 - 실패
+# 제출 답안2 - 성공
+import sys
+input = sys.stdin.readline
+N = int(input())
+input()
+data = list(input().split())
+cand = dict()
+
+for x in data:
+    if x in cand:
+        cand[x]+=1
+        continue
+    elif len(cand) == N:
+        lowValue = min(cand.values())
+        out = [k for k, v in cand.items() if v == lowValue][0]
+        del (cand[out])
+
+    cand[x] = 1
+print(*sorted(map(int,cand.keys())))
+
+
+# 제출 답안1 - 실패(틀렸습니다)
 
 import sys
 input = sys.stdin.readline
@@ -16,7 +40,7 @@ for x in data:
         candidates[x] += 1
     else:
         if len(candidates) >= N:
-            out = time.popleft()
+            out = time.popleft() # <-------
             del(candidates[out])
             time.append(x)
             candidates[x] = 1
