389892afacc5bbc8e01db1b374d8c12bb899b517
Hankyul Kim <71064836+Hankyul-k@users.noreply.github.com>
2022-03-24 20:29:52 +0900

    백준 1018번 체스판 다시 칠하기
    브루트포스 알고리즘

--- silver5/1018_체스판다시칠하기.py
+++ silver5/1018_체스판다시칠하기.py
@@ -2,7 +2,43 @@
 https://www.acmicpc.net/problem/1018
 '''
 
-# 제출 답안
+# 제출 답안 2
+import sys
+input = sys.stdin.readline
+m,n = map(int,input().split())
+matrix = [list(input()) for _ in range(m)]
+matrix = [[1 if matrix[i][j]=='B' else 0 for j in range (n)] for i in range (m)]
+
+def board(x,y):
+    w_corner,b_corner = 0,0
+    check = 0
+    for i in range(x,x+8):
+        check += sum(matrix[i][y:y+8])
+    if check%64 == 0:
+        return 32
+    for i in range(x,x+8):
+        for j in range(y,y+8):
+
+            if matrix[i][j]:
+                if (i+j)%2: 
+                    b_corner +=1  
+                else:
+                    w_corner +=1 
+            else:
+                if (i+j)%2:
+                    w_corner += 1  
+                else:
+                    b_corner += 1  
+    return min(b_corner,w_corner)
+
+
+change = 32
+for i in range(m-7):
+    for j in range(n-7):
+        change = min(change,board(i,j))
+print(change)
+
+# 제출 답안 1
 
 import sys
 input = sys.stdin.readline
